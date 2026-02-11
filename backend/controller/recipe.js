22b83de1c9f4748646d952ba807ca34c86e99345
gregory <gregjeanlouis21@gmail.com>
2026-02-11 16:34:18 -0500

    finshed the backend and full crud operations

--- backend/controller/recipe.js
+++ backend/controller/recipe.js
@@ -1,21 +1,85 @@
-const getRecipes = (req, res) => {
-  res.send("Get all recipes");
+const Recipes = require("../models/recipe");
+
+const getRecipes = async (req, res) => {
+  try {
+    const recipes = await Recipes.find();
+    return res.status(200).json(recipes);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-const getRecipe = (req, res) => {
-  res.send("Get single recipe");
+const getRecipe = async (req, res) => {
+  try {
+    const recipe = await Recipes.findById(req.params.id);
+
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    return res.status(200).json(recipe);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-const addRecipe = (req, res) => {
-  res.send("Add recipe");
+const addRecipe = async (req, res) => {
+  try {
+    const { title, ingredients, instructions, time } = req.body;
+
+    
+    if (!title || !ingredients || !instructions) {
+      return res.status(400).json({
+        message: "Required fields can't be empty",
+      });
+    }
+
+    const newRecipe = await Recipes.create({
+      title,
+      ingredients,
+      instructions,
+      time,
+    });
+
+    return res.status(201).json(newRecipe);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+
+  }
 };
 
-const editRecipe = (req, res) => {
-  res.send("Edit recipe");
+const editRecipe = async (req, res) => {
+  try {
+    const { title, ingredients, instructions, time } = req.body;
+
+    const updatedRecipe = await Recipes.findByIdAndUpdate(
+      req.params.id,
+      { title, ingredients, instructions, time },
+      { new: true }
+    );
+
+    if (!updatedRecipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    return res.status(200).json(updatedRecipe);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-const deleteRecipe = (req, res) => {
-  res.send("Delete recipe");
+const deleteRecipe = async (req, res) => {
+  try {
+    const deletedRecipe = await Recipes.findByIdAndDelete(req.params.id);
+
+    if (!deletedRecipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    return res.status(200).json({ message: "Recipe deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
---
+++ backend/models/recipe.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = mongoose.Schema({
+    title:{
+        type:String,
+        require:true,
+    },
+    ingredients:{
+        type:String,
+        require:true,
+    },
+    instructions:{
+        type:String,
+        require:true,
+    },
+    time:{
+        type:String,
+        require:true,
+    },coverImage:{
+        type:String,
+
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model("Recipes",recipeSchema)
